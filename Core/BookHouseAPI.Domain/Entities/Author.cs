94fcab0002927a87521fe217787d19ec2791dc49
bastille1001 <timur.physics@mail.ru>
2024-03-31 23:10:32 +0400

    test changes locally before pushing them to remote repository

--- Core/BookHouseAPI.Domain/Entities/Author.cs
+++ Core/BookHouseAPI.Domain/Entities/Author.cs
@@ -12,11 +12,7 @@ namespace BookHouseAPI.Domain.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Country { get; set; }
-        //public int Age { get; set; }
-        //public DateTime Birthday { get; set; }
-        //public DateTime RipDate { get; set; }
-        //public string Gender { get; set; }
-        public string Biography { get; set; } 
+        public string? Biography { get; set; } 
         public int BooksCount { get; set; }
         public ICollection<Book> Books { get; set; }
     }
